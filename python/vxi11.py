821f2d33471736c85f5b39e74b5059698d1659af
Roger Light <roger@atchoo.org>
2014-07-15 15:32:53 +0100

    Python fixes.

--- python/vxi11.py
+++ python/vxi11.py
@@ -6,30 +6,46 @@ READ_TIMEOUT = 2000
 NULL_READ_RESP = 50
 NULL_WRITE_RESP = 51
 
+
 class Vxi11(object):
     def __init__(self, address, device="inst0"):
         if address is None or address == "":
             raise ValueError("address must be defined")
 
-        self._clink = None
+        self._clink = c_void_p()
         self._address = address
         self._device = device
-        if(_vxi11_open_device(self._clink, address, device)):
+        if address:
+            a = c_char_p(address.encode())
+        else:
+            a = None
+        if device:
+            d = c_char_p(device.encode())
+        else:
+            d = None
+        if(_vxi11_open_device(byref(self._clink), a, d)):
             raise IOError("unable to open device at address "+address)
 
     def __del__(self):
         self.close()
 
     def close(self):
-        _vxi11_close_device(self._clink, self._address)
-        self._clink = None
-        self._address = None
-        self._device = None
+        if self._clink:
+            if self._address:
+                a = c_char_p(self._address.encode())
+            else:
+                a = None
+            _vxi11_close_device(self._clink, a)
+            self._clink = None
+            self._address = None
+            self._device = None
 
     def send(self, cmd):
-        rc = _vxi11_send(self._clink, cmd, len(cmd))
+        c = cmd.encode()
+        rc = _vxi11_send(self._clink, c_char_p(c), len(c))
         if rc:
             raise IOError("error sending to the device: "+str(rc))
+        return 0
 
     def receive(self, max_length=1024, timeout=READ_TIMEOUT):
         buf = create_string_buffer(max_length)
@@ -38,9 +54,10 @@ class Vxi11(object):
             raise IOError("error receiving from the device: "+str(rc))
         return buf.value
 
-    def send_data_block(self, buf):
-        if _vxi11_send_data_block(self._clink, buf, len(buf)):
+    def send_data_block(self, cmd, buf):
+        if _vxi11_send_data_block(self._clink, cmd.encode(), buf, len(buf)):
             raise IOError("error sending to the device: "+str(rc))
+        return 1
 
     def receive_data_block(self, max_length=1024, timeout=READ_TIMEOUT):
         buf = create_string_buffer(max_length)
@@ -51,7 +68,8 @@ class Vxi11(object):
 
     def send_and_receive(self, cmd, max_length=1024, timeout=READ_TIMEOUT):
         buf = create_string_buffer(max_length)
-        rc = _vxi11_send_and_receive(self._clink, cmd, buf, max_length, timeout)
+        c = cmd.encode()
+        rc = _vxi11_send_and_receive(self._clink, c_char_p(c), buf, max_length, timeout)
         if rc < 0:
             raise IOError("error sending/receiving from the device: "+str(rc))
         return buf.value
@@ -63,7 +81,7 @@ class Vxi11(object):
         return _vxi11_obtain_double_value(self._clink, cmd)
 
 
-_libvxi11 = cdll.LoadLibrary(ctypes.util.find_library("vxi11"))
+_libvxi11 = cdll.LoadLibrary("vxi11.dll")
 
 _vxi11_open_device = _libvxi11.vxi11_open_device
 _vxi11_open_device.argtypes = [POINTER(c_void_p), c_char_p, c_char_p]
@@ -82,7 +100,7 @@ _vxi11_receive.argtypes = [c_void_p, c_char_p, c_size_t, c_ulong]
 _vxi11_receive.restype = c_ssize_t
 
 _vxi11_send_data_block = _libvxi11.vxi11_send_data_block
-_vxi11_send_data_block.argtypes = [c_void_p, c_char_p, c_size_t]
+_vxi11_send_data_block.argtypes = [c_void_p, c_char_p, c_char_p, c_size_t]
 _vxi11_send_data_block.restype = c_int
 
 _vxi11_receive_data_block = _libvxi11.vxi11_receive_data_block
@@ -90,7 +108,7 @@ _vxi11_receive_data_block.argtypes = [c_void_p, c_char_p, c_size_t, c_ulong]
 _vxi11_receive_data_block.restype = c_ssize_t
 
 _vxi11_send_and_receive = _libvxi11.vxi11_send_and_receive
-_vxi11_send_and_receive.argtypes = [c_void_p, c_char_p, c_size_t, c_ulong]
+_vxi11_send_and_receive.argtypes = [c_void_p, c_char_p, c_char_p, c_size_t, c_ulong]
 _vxi11_send_and_receive.restype = c_int
 
 _vxi11_obtain_long_value = _libvxi11.vxi11_obtain_long_value_timeout
