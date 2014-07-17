2cedc82f042ad100c860876abffbe5daf91232a6
Roger Light <roger@atchoo.org>
2014-07-17 10:39:47 +0100

    Python setup.py

---
+++ python/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='vxi11',
+	version='1.12',
+	description='Communication with instruments over VISA/VXI11',
+	url='http://github.com/applied-optics/vxi11',
+	license='GPL 2',
+	py_modules=['vxi11']
+	)
